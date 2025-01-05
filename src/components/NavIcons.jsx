ac57778b5bd39e557f38b2d520b5dfea88c90ed8
Tanvir Ahamed <sowmik265@gmail.com>
2025-01-05 09:32:57 +0600

    search bar done

---
+++ src/components/NavIcons.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+const NavIcons = () => {
+  return (
+    <div>NavIcons</div>
+  )
+}
+
+export default NavIcons
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
+const SearchBar = () => {
+    const router = useRouter();
+    const handleSearch = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") ;
+    if (name) {
+      router.push(`/list?name=${name}`);
+    }
+}
+  return (
+    <form
+      className="flex items-center gap-4 bg-gray-100 p-2 rounded-md flex-1"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Search..."
+        className=" flex-1 bg-transparent outline-none"
+      />
+      <button className=" cursor-pointer">
+        <Image src="/search.png" alt="search" width={16} height={16}></Image>
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
