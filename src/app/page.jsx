69b4576194c991fd7742806ae1e8a7196de069c1
Tanvir Ahamed <sowmik265@gmail.com>
2025-01-13 12:49:33 +0600

    categories components done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 import React from "react";
@@ -6,9 +7,15 @@ const HomePage = () => {
   return (
     <div className="">
       <Slider></Slider>
-      <div className=" mt-24 px-4 md:px-8 lgp:px-16 xl:px-32 2xl:px-64">
+      <div className=" mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className=" text-2xl">Featured Products</h1>
         <ProductList></ProductList>
+      </div>
+      <div className=" mt-24 ">
+        <h1 className=" text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
+          Categories
+        </h1>
+        <CategoryList></CategoryList>
       </div>
     </div>
   );
