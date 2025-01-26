f5960e35c2d6c892fcae0354a3d1902cedd5e970
Tanvir Ahamed <sowmik265@gmail.com>
2025-01-26 10:02:12 +0600

    product customization component created

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,3 +1,5 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
 
 const SinglePage = () => {
@@ -8,7 +10,48 @@ const SinglePage = () => {
         <ProductImages></ProductImages>
       </div>
       {/* TEXT */}
-      <div className="w-full lg::w-1/2 flex flex-col gap-6">TEXTS</div>
+      <div className="w-full lg::w-1/2 flex flex-col gap-6">
+        <h1 className=" text-4xl font-medium">Product name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          consequuntur libero recusandae id tenetur. Nulla, totam nihil
+          praesentium pariatur facilis rem cum consectetur quidem fuga earum,
+          quasi sapiente deserunt! Nam?
+        </p>
+        <div className=" h-[2px] bg-gray-100" />
+        <div className=" flex items-center gap-4">
+          <h3 className=" text-xl text-gray-500 line-through">$59</h3>
+          <h2 className=" font-medium text-2xl">$49</h2>
+        </div>
+        <div className=" h-[2px] bg-gray-100" />
+        <CustomizeProducts></CustomizeProducts>
+        <Add></Add>
+        <div className=" h-[2px] bg-gray-100" />
+        <div className=" text-sm">
+          <h4 className=" font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            asperiores accusamus hic doloribus molestiae ipsum sint
+            reprehenderit, repellat voluptate omnis rerum ipsa expedita a vel!
+          </p>
+        </div>
+        <div className=" text-sm">
+          <h4 className=" font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            asperiores accusamus hic doloribus molestiae ipsum sint
+            reprehenderit, repellat voluptate omnis rerum ipsa expedita a vel!
+          </p>
+        </div>
+        <div className=" text-sm">
+          <h4 className=" font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            asperiores accusamus hic doloribus molestiae ipsum sint
+            reprehenderit, repellat voluptate omnis rerum ipsa expedita a vel!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
